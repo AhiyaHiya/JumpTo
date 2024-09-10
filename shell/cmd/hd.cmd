de1090bac470d2384d9cdfccbc901f9faa50809c
Jaime Rios <3794310+AhiyaHiya@users.noreply.github.com>
2024-09-09 23:14:27 -0700

    Update shell cmd

--- shell/cmd/hd.cmd
+++ shell/cmd/hd.cmd
@@ -14,6 +14,24 @@ REM Check if HyperDrive.exe is available on PATH
 
 REM Call HyperDrive.exe with the arguments passed to the script
 echo WARNING, we are using a path when we should be relying on PATH variable
-C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug\HyperDrive.exe %*
 
-endlocal
+REM Check if the argument is -g or --get
+if "%1"=="-g" (
+    goto :CallGet
+) else if "%1"=="--get" (
+    goto :CallGet
+) else (
+    REM Call HyperDrive.exe with the arguments passed to the script
+    C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug\HyperDrive.exe %*
+)
+
+goto :EOF
+
+:CallGet
+    REM Capture the output of HyperDrive.exe
+    for /f "delims=" %%i in ('C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug\HyperDrive.exe %*') do (
+        REM Use the output as a path to CD to
+        cd /d "%%i"
+    )
+    exit /b
+goto :EOF
