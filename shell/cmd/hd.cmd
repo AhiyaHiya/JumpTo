78055b0171082b2dd0838eb09438b6ef71dcc9d0
Jaime Rios <3794310+AhiyaHiya@users.noreply.github.com>
2024-10-03 22:24:50 -0700

    Added verbose output to hd.cmd

--- shell/cmd/hd.cmd
+++ shell/cmd/hd.cmd
@@ -2,35 +2,45 @@
 
 setlocal
 
-REM Add the path to the environment PATH variable
+:: Print all arguments passed to the script
+echo Arguments: %*
+
+:: Add the path to the environment PATH variable
 setx PATH "%PATH%;C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug"
 
-REM Check if HyperDrive.exe is available on PATH
-@REM where HyperDrive.exe >nul 2>&1
-@REM if %errorlevel% neq 0 (
-@REM     echo Error: HyperDrive.exe not found on PATH.
-@REM     exit /b 1
-@REM )
+:: Check if HyperDrive.exe is available on PATH
+:: where HyperDrive.exe >nul 2>&1
+:: if %errorlevel% neq 0 (
+::     echo Error: HyperDrive.exe not found on PATH.
+::     exit /b 1
+:: )
 
-REM Call HyperDrive.exe with the arguments passed to the script
-echo WARNING, we are using a path when we should be relying on PATH variable
+:: Call HyperDrive.exe with the arguments passed to the script
+:: echo WARNING, we are using a path when we should be relying on PATH variable
 
-REM Check if the argument is -g or --get
+set HyperDrivePath=C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug\HyperDrive.exe
+
+:: Check if the argument is -g or --get
 if "%1"=="-g" (
+    echo Calling HyperDrive.exe with -g
     goto :CallGet
 ) else if "%1"=="--get" (
+    echo Calling HyperDrive.exe with --get
     goto :CallGet
 ) else (
-    REM Call HyperDrive.exe with the arguments passed to the script
-    C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug\HyperDrive.exe %*
+    echo Calling HyperDrive.exe with %*
+    :: Call HyperDrive.exe with the arguments passed to the script
+    %HyperDrivePath% %*
 )
 
 goto :EOF
 
 :CallGet
-    REM Capture the output of HyperDrive.exe
-    for /f "delims=" %%i in ('C:\Users\jaime\Development\GitHub\HyperDrive\build\Debug\HyperDrive.exe %*') do (
-        REM Use the output as a path to CD to
+    echo Arguments: %*
+    :: Capture the output of HyperDrive.exe
+    for /f "delims=" %%i in ('%HyperDrivePath% %*') do (
+        :: Use the output as a path to CD to
+        echo Changing directory to "%%i"
         cd /d "%%i"
     )
     exit /b
